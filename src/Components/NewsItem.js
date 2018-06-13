2d73ae25fd2b86d315fe5a0e1d54ef58515be38d
MoustafaBayommy <moustafabayommy@gmail.com>
2018-06-13 02:43:42 +0200

    create NewItem Component

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,15 +1,75 @@
 import React,{ Component } from 'react';
-import {TouchableOpacity} from 'react-native';
+import PropTypes from 'prop-types';
 
+import {
+View,
+TouchableOpacity,
+StyleSheet
+} from 'react-native';
+import Thumble from './Thumble';
+import Byline from './Byline';
+import AppText from './AppText';
+import * as globalStyles from '../styles/global';
 
 
 
 export default class extends Component{
 
 
-    render=()=>(
-<TouchableOpacity>
-
+    render=()=>{
+        const {
+            style,
+            imageUrl,
+            title,
+            author,
+            date,
+            location,
+            description,
+            } = this.props;
+        return(
+<TouchableOpacity
+style={style}
+>
+<View>
+<Thumbnail
+url={imageUrl}
+titleText={title}
+accentColor={accentColor}
+style={styles.thumbnail}
+/>
+<View style={styles.content}>
+<Byline
+author={author}
+date={date}
+location={location}
+/>
+<AppText>
+{description}
+</AppText>
+</View>
+</View>
     </TouchableOpacity>
-    )
+    )}
 }
+
+NewsItem.propTypes = {
+    imageUrl: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    date: PropTypes.instanceOf(Date).isRequired,
+    author: PropTypes.string.isRequired,
+    location: PropTypes.string,
+    index: PropTypes.number.isRequired,
+    onPress: PropTypes.func.isRequired,
+    style: View.propTypes.style
+    };
+
+
+    const styles = StyleSheet.create({
+        thumbnail: {
+        marginBottom: 5
+        },
+        content: {
+        paddingHorizontal: 5
+        }
+        });
