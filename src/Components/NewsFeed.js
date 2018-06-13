954dc7ba3233c9c0ac6327df1a7d3d433df5abce
Moustafa Bayommy <moustafabayommy@gmail.com>
2018-06-13 11:55:15 +0200

    add List View To news feedback

---
+++ src/Components/NewsFeed.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import {
+    View,
+    ListView,
+    StyleSheet
+} from 'react-native';
+
+
+import { NewsItem } from './NewsItem'
+
+import * as globalStyles from '../styles/global';
+
+export default class NewsFeed extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.ds = ListView.DataSource({
+            rowHasChanged: (row1, row2) => row1.title !== row2.title
+        });
+
+        this.state = { dataSource: this.ds.cloneWithRows(props.news) };
+    }
+
+    render() {
+
+        return (
+            <View style={globalStyles.COMMON_STYLES.pageContainer}>
+
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={this.renderRow}
+                    style={this.props.listStyles}
+                />
+
+            </View>
+        )
+    }
+
+
+    renderRow(rowData, ...rest) {
+        const index = parseInt(rest[1], 10);
+        return (
+            <NewsItem
+                index={index}
+                style={styles.newsItem}
+                {...rowData}
+
+            />
+        )
+    }
+
+
+}
+
+NewsFeed.propTypes = {
+    news: PropTypes.arrayOf(PropTypes.object),
+    listStyles: View.propTypes.style
+}
+
+
+
+NewsFeed.defaultProps = {
+    news: [{
+        title: 'React Native',
+        imageUrl: 'https://facebook.github.io/react/img/logo_og.png',
+        description: 'Build Native Mobile Apps using JavaScript and React',
+        date: new Date(),
+        author: 'Facebook',
+        location: 'Menlo Park, California',
+        url: 'https://facebook.github.io/react-native'
+    }, {
+        title: 'Packt Publishing',
+        imageUrl: 'https://www.packtpub.com/sites/default/files/packt_logo.png',
+        description: 'Stay Relevant',
+        date: new Date(),
+        author: 'Packt Publishing',
+        location: 'Birmingham, UK',
+        url: 'https://www.packtpub.com/'
+    }]
+};
+
+
+const styles = StyleSheet.create({ newsItem: { marginBottom: 20 } });
+
